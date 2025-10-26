af3d9c7372eafaea3d0a2e940f1dc93ced324754
Manuel Guerrero Naranjo <manuel.guerrero.nar@gmail.com>
2025-10-26 13:49:34 +0100

    Restaurado todo

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,13 @@
-//Animación lupa
-
 const botonLupa = document.querySelector(".botonBuscar");
+const barraBusqueda = document.querySelector(".barraBusqueda");
 
 botonLupa.addEventListener("click", () => {
   botonLupa.classList.add("animar-lupa");
+  barraBusqueda.classList.toggle("mostrar");
 
-  // Quita la clase al acabar la animación para que pueda repetirse
   botonLupa.addEventListener(
     "animationend",
-    () => {
-      botonLupa.classList.remove("animar-lupa");
-    },
+    () => botonLupa.classList.remove("animar-lupa"),
     { once: true }
   );
 });
